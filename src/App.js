395dcccc9f41bd3dc9c3a62fc3e38145f175780d
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-08-03 00:38:00 +0300

    Add js particles and background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Logo from './components/logo/logo.component';
 import './App.css';
+
+const particlesOptions={
+  particles: {
+    number: {
+      value: 120,
+      density:{
+        enable: true,
+        value_area: 1000
+      }
+    } 
+},
+"interactivity": {
+  "detect_on": "window",
+  "events": {
+    "onhover": {
+      "enable": true,
+      "mode": "repulse"
+    }
+  }
+ }
+}
 
 class App extends Component{
   constructor(){
     super();
+
+    this.state = {
+
+    }
   }
   
   render(){
     return(
-      <div>
+      <div className='App'>
+        <Particles 
+          className='particles'
+          params={particlesOptions}
+        />
         <Logo/>
       </div>
     )
